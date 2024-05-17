93be2acc173fba8eabebefd1dbb8869fc8829ba3
joaodetoni77 <joaodetoni123@gmail.com>
2024-05-17 20:42:48 -0300

    Início da alteração no projeto para atender as demandas de câmera e integração com pagamento

---
+++ src/pages/camera/index.tsx
@@ -0,0 +1,66 @@
+import { CameraCapturedPicture, CameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+//export default function App() {
+export const Camera = (props: CameraCapturedPicture) => {
+    const [facing, setFacing] = useState('back');
+    const [permission, requestPermission] = useCameraPermissions();
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: 'center' }}>Você precisa de permissão para acessar a câmera!</Text>
+                <Button onPress={requestPermission} title="Fornecer Permissão" />
+            </View>
+        );
+    }
+
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+
+    return (
+        <View style={styles.container}>
+            <CameraView style={styles.camera} facing={facing}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <Text style={styles.text}>Icone</Text>
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+});
